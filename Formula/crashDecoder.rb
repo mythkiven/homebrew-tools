c67a27bc6f7404067018f172529c34837021b81c
kiven <mythziven@gmail.com>
2025-03-20 11:20:13 +0800

    Add crashDecoder formula

--- Formula/crashDecoder.rb
+++ Formula/crashDecoder.rb
@@ -1,8 +1,12 @@
 class Crashdecoder < Formula
+  # 注意，这里的命名规则不是驼峰。而是，首字母大写，其余小写。
+  # 否则会把报错
+    # Error: No available formula with the name "mythkiven/tools/crashdecoder". Did you mean mythkiven/tools/crashDecoder?
+    # In formula file: /opt/homebrew/Library/Taps/mythkiven/homebrew-tools/Formula/crashdecoder.rb
     desc "iOS崩溃日志分析和符号化工具"
     homepage "https://github.com/mythkiven/HomebrewScript/tree/main/iOSCrashDecoder-1.0.0"
     url "https://github.com/mythkiven/HomebrewScript/tree/main/iOSCrashDecoder-1.0.0/archive/v1.0.0.tar.gz"
-    sha256 "6a1d1da4d0773fd6da5ce625e49869bdb9ef8d8dbc495208326c9e62b470cc7b"
+    sha256 "72c823899c9b3320cdae8677e36bac5f12d85026b0d4e7b45d8eb906cdabf3f0"
     license "MIT"
     
     def install
